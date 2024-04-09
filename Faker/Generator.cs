8a773edd32cf7b657ee10e5681f11297db3c66bc
AndreKlim1 <andreyklimovich159@gmail.com>
2024-04-09 15:13:43 +0300

    Generator Complete

--- Faker/Generator.cs
+++ Faker/Generator.cs
@@ -10,6 +10,7 @@ namespace Faker
     {
         const int PRINTABLE_START = 32;
         const int PRINTABLE_END = 126;
+        const int DAY_SECONDS = 86400;
 
         private static Random _random = new Random();
         private static readonly Dictionary<Type, Func<object>> _basicTypes = new Dictionary<Type, Func<object>>()
@@ -42,7 +43,12 @@ namespace Faker
 
         private static object GetRandomDateTime()
         {
-            throw new NotImplementedException();
+            var dateTime = new DateTime(1, 1, 1);
+            int days = _random.Next(2000000);
+            int time = _random.Next(DAY_SECONDS);
+            dateTime = dateTime.AddDays(days);
+            dateTime = dateTime.AddSeconds(time);
+            return dateTime;
         }
 
         private static object GetRandomBool()
@@ -79,7 +85,35 @@ namespace Faker
 
         private static object GetRandomList(Type type)
         {
-            throw new NotImplementedException();
+            var listType = typeof(List<>).MakeGenericType(type);
+            var list = Activator.CreateInstance(listType);
+
+            int amount = _random.Next(1, 10);
+
+            var addMethod = listType.GetMethod("Add");
+
+            for (int i = 0; i < amount; i++)
+            {
+                addMethod.Invoke(list, new object[] { GenerateRandomPart(type) });
+            }
+
+            return list;
+        }
+
+        public static object GenerateRandomPart(Type type)
+        {
+            if (_basicTypes.ContainsKey(type))
+            {
+                return _basicTypes[type].Invoke();
+            }
+            else if (type.IsGenericType)
+            {
+                var genericType = type.GetGenericTypeDefinition();
+                var elementType = type.GetGenericArguments();
+                if (_collectionTypes.ContainsKey(genericType))
+                    return _collectionTypes[genericType].Invoke(elementType[0]);
+            }
+            return Activator.CreateInstance(type);
         }
     }
 }
